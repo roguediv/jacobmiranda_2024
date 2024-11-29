b78339d420177705150a932b5e747a025a8d9fea
roguediv <47837581+roguediv@users.noreply.github.com>
2024-11-29 14:14:47 -0600

    async compatability

--- app/projects/[title]/page.tsx
+++ app/projects/[title]/page.tsx
@@ -10,8 +10,11 @@ import {ProjectData} from "@/scripts/data/ProjectData"
 import { Project } from "@/scripts/data/Project";
 import Image from "next/image"
 
-export default function page({params}: {params: {title: string}}) {
-  let proj : Project | undefined = ProjectData.find((array : Project) => array.body.title === params.title);
+export default async function page(props: {params: Promise<{title: string}>}) {
+  const params = await props.params;
+  const title : string = params.title as string;
+
+  let proj : Project | undefined = ProjectData.find((array : Project) => array.body.title === title);
   if (proj == undefined) {
     return (
     <div className={`pge-project projectHome`}>
@@ -46,11 +49,11 @@ export default function page({params}: {params: {title: string}}) {
   }
   return (
     <div className={`pge-project ${proj.body.theme} project-${proj.body.title}`}>
-      <Header page="project" title={params.title} />
+      <Header page="project" title={title} />
       <ProjectSct1 title={proj.sct1.title} overview={proj.sct1.overview} timeframe={proj.sct1.timeframe} technologies={proj.sct1.technologies} />
-      <ProjectSct2 title={params.title} header={proj.sct2.header} desc={proj.sct2.desc} card1={proj.sct2.card1} card2={proj.sct2.card2} />
-      <ProjectSct3 title={params.title} header1={proj.sct3.header1} desc1={proj.sct3.desc1} header2={proj.sct3.header2} desc2={proj.sct3.desc2} />
-      <ProjectSct4 title={params.title} header={proj.sct4.header} desc={proj.sct4.desc} />
+      <ProjectSct2 title={title} header={proj.sct2.header} desc={proj.sct2.desc} card1={proj.sct2.card1} card2={proj.sct2.card2} />
+      <ProjectSct3 title={title} header1={proj.sct3.header1} desc1={proj.sct3.desc1} header2={proj.sct3.header2} desc2={proj.sct3.desc2} />
+      <ProjectSct4 title={title} header={proj.sct4.header} desc={proj.sct4.desc} />
       <ProjectSct5 header={proj.sct5.header} items={proj.sct5.items} />
       <CtaCard />
       <section className="moreProjects">
