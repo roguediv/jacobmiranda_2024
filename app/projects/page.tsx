210d8e7399b106316be777a75bc612dd226ff813
roguediv <47837581+roguediv@users.noreply.github.com>
2024-11-29 14:28:48 -0600

    projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,12 +15,12 @@ export const metadata: Metadata = {
   description: 'A portfolio of Jacob\'s projects.',
 }
 
-export default function page({params}: {params: {title: string}}) {
-  let proj : Project | undefined = ProjectData.find((array : Project) => array.body.title === params.title);
+export default function page() {
+  let proj : Project | undefined = ProjectData.find((array : Project) => array.body.title === "");
   if (proj == undefined) {
     return (
     <div className={`pge-project projectHome`}>
-      <Header page="project" title={params.title} />
+      <Header page="project" title={""} />
       <section className="sct-1">
         <div className="sct-content">
           <ProjectCards className="default" projects={ProjectData} />
@@ -36,20 +36,6 @@ export default function page({params}: {params: {title: string}}) {
     </div>);
   }
   return (
-    <div className={`pge-project ${proj.body.theme} project-${proj.body.title}`}>
-      <Header page="project" title={params.title} />
-      <ProjectSct1 title={proj.sct1.title} overview={proj.sct1.overview} timeframe={proj.sct1.timeframe} technologies={proj.sct1.technologies} />
-      <ProjectSct2 title={params.title} header={proj.sct2.header} desc={proj.sct2.desc} card1={proj.sct2.card1} card2={proj.sct2.card2} />
-      <ProjectSct3 title={params.title} header1={proj.sct3.header1} desc1={proj.sct3.desc1} header2={proj.sct3.header2} desc2={proj.sct3.desc2} />
-      <ProjectSct4 title={params.title} header={proj.sct4.header} desc={proj.sct4.desc} />
-      <ProjectSct5 header={proj.sct5.header} items={proj.sct5.items} />
-      <CtaCard />
-      <section className="moreProjects">
-        <div className="sct-content">
-          <ProjectCards projects={ProjectData} />
-        </div>
-      </section>
-      
-    </div>
+    <></>
   );
 }
